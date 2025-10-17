5adbd70a90db41c2f8e995164c093344c99a354f
Faraz5658 <farazahmad5623@gmail.com>
2025-10-17 18:51:59 +0500

    Logical Operators

---
+++ Logical Operators/Logical Operators.py
@@ -0,0 +1,14 @@
+# 🌟 Logical Operators Example
+
+a = 10
+b = 5
+c = 2
+
+# AND operator (True if both conditions true)
+print("AND operator:", a > b and b > c)   # True
+
+# OR operator (True if any one condition true)
+print("OR operator:", a < b or b > c)     # True
+
+# NOT operator (Negates the result)
+print("NOT operator:", not(a > b))        # False
