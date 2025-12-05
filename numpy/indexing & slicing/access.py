726e24bd56c398179681c077fb99aa477b1bf333
Faraz5658 <farazahmad5623@gmail.com>
2025-12-05 08:44:08 +0500

    access arrays

---
+++ numpy/indexing & slicing/access.py
@@ -0,0 +1,7 @@
+import numpy as np
+arr=np.array([1,2,3,4,5,6,7,8,9])
+# Accessing elements using positive indexing
+print(arr[0])# assessing first element
+print(arr[4])# accessing fifth element
+print(arr[-1])# assessing last element
+print(arr[-3])# accessing third last element
