65050bdb9a0672ae16d62ed362a87df5db65283c
Faraz5658 <farazahmad5623@gmail.com>
2025-12-15 21:19:57 +0500

    line plot with style

---
+++ Pandas/Matplotlib/LinePlotWithStyle.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+x=[1,2,3,4,5]
+y=[1000,2000,4000,1500,3000]
+plt.plot[x,y, color='pink', linestyle="--",marker="o"]
+plt.title("SIMPLE LINE PLOT With Style")
+plt.xlabel("X VLAUE")
+plt.ylabel('Y VALUE')
+plt.show()
