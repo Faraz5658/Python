9fca2e070d496c6adf57eca016e37967fb66fbca
Faraz5658 <farazahmad5623@gmail.com>
2025-12-15 21:13:56 +0500

    simple line plot

---
+++ Pandas/Matplotlib/SimpleLinePlot.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+x=[1,2,3,4,5,6,7]
+value=[23,45,56,34,23,23,12]
+
+plt.plot(x,value)
+plt.title("SIMPLE LINE PLOT")
+plt.xlabel("X VLAUE")
+plt.ylabel('Y VALUE')
+plt.show()
