48732fcd56045925537eb9a8b0a3751d0c835086
Faraz5658 <farazahmad5623@gmail.com>
2025-12-15 21:27:44 +0500

    sctter Plot

---
+++ Pandas/Matplotlib/ScatterPlot.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+x = [5, 7, 8, 9, 4]
+y = [10, 12, 15, 20, 5]
+plt.scatter(x, y) # Draws individual points
+plt.title("Scatter Plot Example")
+plt.xlabel("X Values")
+plt.ylabel("Y Values")
+plt.show()
